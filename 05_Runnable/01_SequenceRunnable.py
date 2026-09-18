797f3db1fc6ff64b7b814ddd3aecc34a40d903ce
SubhasishElixor-HQ <subhasishsahoo4747@gmail.com>
2026-09-18 21:01:07 +0530

    Add: SequenceRunnable use

---
+++ 05_Runnable/01_SequenceRunnable.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
+# 1. Prompt Template
+prompt = ChatPromptTemplate.from_template(
+    "Explain {topic} in simple words"
+)
+
+# 2. Model
+# 2. Create Hugging Face LLM
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-R1-0528",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+# 3. Output Parser
+parser = StrOutputParser()
+
+
+chain = prompt | model | parser
+
+result = chain.invoke("Machine Learning")
+print(result)

e21ec3adfc29ec66ae98c8839cd24631dea1c715
SubhasishElixor-HQ <subhasishsahoo4747@gmail.com>
2026-09-19 09:25:47 +0530

    Add: RunnablePassThrough

---
+++ 05_Runnable/03_RunnablePassThrough.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+
+
+# Components
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-R1-0528",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+parser = StrOutputParser()
+
+code_prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a code generator"),
+    ("human", "{topic}")
+])
+
+explain_prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful assistant who explains code in simple terms"),
+    ("human", "Explain the following code in simple words:\n{code}")
+])
+
+seq = code_prompt | model | parser 
+
+
+seq2 = RunnableParallel(
+    {"code" :  RunnablePassthrough(),
+     "explanation" : explain_prompt | model | parser
+    }
+)
+
+chain = seq | seq2
+
+result = chain.invoke({"topic" : "please write a code of palindrome in python "})
+
+print(result['code'])
+print(result['explanation'])

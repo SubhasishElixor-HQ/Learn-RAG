1769434c03906a0dd58c13aa700adb4e608b05f1
SubhasishElixor-HQ <subhasishsahoo4747@gmail.com>
2026-09-19 09:24:49 +0530

    Add: ParallelRunnable

---
+++ 05_Runnable/02_ParallelRunnable.py
@@ -0,0 +1,41 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel,RunnableLambda
+
+# Components
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-R1-0528",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+parser = StrOutputParser()
+
+# Two different prompts
+short_prompt = ChatPromptTemplate.from_template(
+    "Explain {topic} in 1-2 lines"
+)
+
+detailed_prompt = ChatPromptTemplate.from_template(
+    "Explain {topic} in detail"
+)
+
+# Input
+topic = "Machine Learning"
+
+chain = RunnableParallel({
+    "short" :RunnableLambda(lambda x :x['short']) |short_prompt | model | parser ,
+    "detailed" :RunnableLambda(lambda x: x['detailed']) |detailed_prompt |model |parser
+})
+
+result = chain.invoke({
+    "short" : {"topic":"Machine Learning"},
+    "detailed" : {"topic":"Deep Learning"}
+})
+
+print(result['short'])
+print(result['detailed'])
